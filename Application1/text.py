f0a4dec7c666e90c8e69326cf3785bd5442bb346
Akash <sir.akashaman@gmail.com>
2021-01-20 23:19:03 +0530

    commit

--- Application1/text.py
+++ Application1/text.py
@@ -1,2 +1,9 @@
-list1 = [3, 5, 15, 20, 4]
-print(min(list1))
+n = int(input("enter the no of subjects :"))
+subMarks = []
+credit = []
+for i in range(n):
+    subMarks[i] = int(input("enter the marks"))
+    credit[i] = int(input("enter the credit "))
+
+for i in range(n):
+    print(subMarks[i])
